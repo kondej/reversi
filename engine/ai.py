411acd16ef8e116129d9314033007404afceb48b
Piotr Kondej <piotr.kondej1111@gmail.com>
2025-06-06 12:29:19 +0200

    Add simple AI

--- engine/ai.py
+++ engine/ai.py
@@ -1,3 +1,16 @@
+import random
+
+
 class AI:
     def __init__(self):
         pass
+
+    # Wykonaj ruch
+    #
+    def get_move(self, game):
+        valid_moves = game.get_valid_moves(2)
+
+        if not valid_moves:
+            return None
+
+        return random.choice(valid_moves)
